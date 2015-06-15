7091b04c73dac931946a02e9e30c7c6bba124bd7
Yuka Kato <y-kato@esm.co.jp>
2015-06-16 01:31:39 +0900

    Render flash messages, and pass the test

---
+++ features/step_definitions/register.rb
@@ -0,0 +1,23 @@
+もし(/^登録ページを表示する$/) do
+  visit '/exhibits/new'
+end
+
+もし(/^タイトルに "(.*?)" と入力する$/) do |title|
+  fill_in 'タイトル', with: title
+end
+
+もし(/^説明文に "(.*?)" と入力する$/) do |description|
+  fill_in '説明', with: description
+end
+
+もし(/^種類として "(.*?)" を選択する$/) do |type|
+  select type, from: '種類' 
+end
+
+もし(/^登録ボタンを押す$/) do
+  click_on '登録する'
+end
+
+ならば(/^"(.*?)" と表示されていること$/) do |message|
+  expect(page).to have_css '.alert-box', text: message
+end
