659c59ce546701924c1cadc88f8ff56044bfae05
Yuka Kato <y-kato@esm.co.jp>
2015-06-20 16:35:12 +0900

    Remove set_exhibits for select box

--- app/controllers/exhibits_controller.rb
+++ app/controllers/exhibits_controller.rb
@@ -1,10 +1,7 @@
 class ExhibitsController < ApplicationController
-  # FIXME create で必要なのは、save が失敗したときだけ（new に戻るときだけ）なんだけど、
-  #       before_action で書いておいていいものなのか。。
-  before_action :set_exhibits, only: %i(new create)
-
   def new
-    @exhibit = Exhibit.new
+    @exhibits = Exhibit.subclasses.map(&:to_s)
+    @exhibit  = Exhibit.new
   end
 
   def create
@@ -14,6 +11,7 @@ class ExhibitsController < ApplicationController
       @exhibit = @exhibit.becomes(Exhibit)
       redirect_to @exhibit, notice: '登録が完了しました。'
     else
+      @exhibits = Exhibit.subclasses.map(&:to_s)
       render :new
     end
   end
@@ -26,9 +24,5 @@ class ExhibitsController < ApplicationController
 
   def exhibit_params
     params.require(:exhibit).permit(:title, :description, :type)
-  end
-  def set_exhibits
-    Dir.glob(File.join(Rails.root, "app", "models", "**", "*.rb")).each {|f| require f}
-    @exhibits = Exhibit.subclasses.map(&:to_s)
   end
 end
