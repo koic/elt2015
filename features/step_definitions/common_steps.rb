a6fa029a3da6323fd419d5f09ff22508a6badf1a
Yuka Kato <y-kato@esm.co.jp>
2015-06-23 23:02:02 +0900

    Add signout test

--- features/step_definitions/common_steps.rb
+++ features/step_definitions/common_steps.rb
@@ -1,29 +1,5 @@
-前提(/^GitHub アカウント "(.*?)" がある$/) do |account|
-  OmniAuth.config.test_mode = true
-
-  OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({
-    provider: 'github',
-    uid: '123456',
-    info: {
-      nickname: "#{account}"
-    }
-  })
-end
-
-前提(/^サインインしている$/) do
-  steps <<-EOS
-    前提 GitHub アカウント "sherlock" がある
-    もし サインインページを表示する
-    かつ "GitHub" でサインインする
-  EOS
-end
-
-もし(/^サインインページを表示する$/) do
-  visit '/signin'
-end
-
-もし(/^"(.*?)" でサインインする$/) do |provider|
-  click_on "#{provider} でサインイン" 
+もし(/^トップページを表示する$/) do
+  visit '/'
 end
 
 ならば(/^"(.*?)" というメッセージが表示されていること$/) do |message|
---
+++ features/step_definitions/user_session_steps.rb
@@ -0,0 +1,35 @@
+前提(/^GitHub アカウント "(.*?)" がある$/) do |account|
+  OmniAuth.config.test_mode = true
+
+  OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({
+    provider: 'github',
+    uid: '123456',
+    info: {
+      nickname: "#{account}"
+    }
+  })
+end
+
+前提(/^サインインしている$/) do
+  steps <<-EOS
+    前提 GitHub アカウント "sherlock" がある
+    もし サインインページを表示する
+    かつ "GitHub" でサインインする
+  EOS
+end
+
+もし(/^サインインページを表示する$/) do
+  visit '/signin'
+end
+
+もし(/^"(.*?)" でサインインする$/) do |provider|
+  click_on "#{provider} でサインイン"
+end
+
+もし(/^サインアウトをクリックする$/) do
+  click_on 'サインアウト'
+end
+
+ならば(/^サインインページが表示されていること$/) do
+  expect(current_path).to eq signin_path
+end
