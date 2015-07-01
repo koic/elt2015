bec0efc410cb9729a315b76855da8d94d23f7d88
Yuka Kato <y-kato@esm.co.jp>
2015-07-01 22:10:15 +0900

    wip[ci skip]

--- app/controllers/user/votes_controller.rb
+++ app/controllers/user/votes_controller.rb
@@ -1,5 +1,22 @@
 class User::VotesController < ApplicationController
+  permits :exhibit_id
+
   def new
+    @vote = current_user.votes.new
+  end
+
+  def create(vote)
+    # TODO exhibit_id が存在しないものを参照してほしくない。。
+    #      今あるものをリスト化してそこから選んでもらうようにすれば平気だと思うけど、
+    #      「存在しない ID を入れられたらエラーにする」みたいなことしたほうがいいのかな。。
+    @vote = current_user.votes.new(vote.merge(user_id: current_user.id))
+
+    if @vote.save
+      # TODO リダイレクト先とメッセージは未定のためてきとう
+      redirect_to exhibits_path, notice: '投票が完了しました。'
+    else
+      render :new
+    end
   end
 
   def what_to_vote
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,5 @@
 class Vote < ActiveRecord::Base
   belongs_to :user
   belongs_to :exhibit
+  validates :exhibit_id, uniqueness: {scope: %i(user_id)}
 end
