3646aeb8cd0d05bef3069f0c55c1fdf774d63dfe
Yuka Kato <y-kato@esm.co.jp>
2015-07-03 01:56:35 +0900

    Add tests not to vote to same exhibit

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,5 +3,5 @@ class Vote < ActiveRecord::Base
   belongs_to :exhibit
 
   validates :user_id, presence: true
-  validates :exhibit_id, presence: true
+  validates :exhibit_id, presence: true, uniqueness: {scope: %i(user_id)}
 end
