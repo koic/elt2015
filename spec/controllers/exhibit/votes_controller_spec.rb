1387a4b7fdf6178eabb79f4f6722364174a990f4
Yuka Kato <y-kato@esm.co.jp>
2015-07-03 00:24:30 +0900

    Add controller spec

---
+++ spec/controllers/exhibit/votes_controller_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe Exhibit::VotesController, type: :controller do
+  before do
+    controller.stub(:current_user){ FactoryGirl.create(:user, :sherlock) }
+  end
+
+  describe 'GET #new' do
+    it '正常にアクセスできること' do
+      get :new
+      expect(response.status).to eq 200
+    end
+  end
+
+  describe 'POST #create' do
+    let(:irene) {FactoryGirl.create(:user, :irene)}
+    let(:food) {FactoryGirl.create(:food, user_id: irene.id)}
+
+    before do
+      @params = {exhibit_id: food.id}
+    end
+
+    context 'POST #create' do
+      it '対象物の得票数が増えること' do
+        expect(food.votes.count).to eq 0
+
+        expect{
+          post :create,  vote: @params
+        }.to change(food.votes, :count).by(1)
+      end
+    end
+  end
+end
