f97ceffbb5034ccf65b9474e0197910c2c29c202
tatsuo.sakurai <tatsuo.sakurai@gmail.com>
2015-07-05 19:04:39 +0900

    Use `to:` instead of `=>`

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root 'exhibits#index'
 
-  get '/auth/:provider/callback' => 'user_sessions#create'
-  get 'signin' =>  'user_sessions#new'
-  delete 'signout' => 'user_sessions#destroy'
+  get '/auth/:provider/callback', to: 'user_sessions#create'
+  get 'signin',                   to: 'user_sessions#new'
+  delete 'signout',               to: 'user_sessions#destroy'
 
   resources :exhibits, only: %i(new create show index)
 
