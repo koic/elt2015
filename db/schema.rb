208db61a3ab4f1416511b31db9ec3be74b58281f
Yuka Kato <y-kato@esm.co.jp>
2015-07-11 16:01:14 +0900

    票数のためのカウンターキャッシュを使うようにした

--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150704030951) do
+ActiveRecord::Schema.define(version: 20150711063239) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "exhibits", force: :cascade do |t|
-    t.string   "title",       null: false
+    t.string   "title",                   null: false
     t.text     "description"
-    t.string   "type",        null: false
-    t.integer  "user_id",     null: false
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.string   "type",                    null: false
+    t.integer  "user_id",                 null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.integer  "votes_count", default: 0
   end
 
   add_index "exhibits", ["user_id"], name: "index_exhibits_on_user_id", using: :btree
