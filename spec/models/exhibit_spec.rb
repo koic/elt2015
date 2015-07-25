e0ef755f020f128468b5a95fd730e658619cefac
tatsuo.sakurai <tatsuo.sakurai@gmail.com>
2015-07-25 13:45:08 +0900

    Add rank border number to Exibit.by_rank

--- spec/models/exhibit_spec.rb
+++ spec/models/exhibit_spec.rb
@@ -7,22 +7,23 @@ RSpec.describe Exhibit, type: :model do
     let!(:food_meat)   { create(:food, title: 'meat') }
     let!(:food_fish)   { create(:food, title: 'fish') }
     let!(:food_potato) { create(:food, title: 'potato') }
+    let!(:food_sake)   { create(:food, title: 'sake') }
 
     before do
-      create(:vote, exhibit_id: food_meat.id)
-
-      create(:vote, exhibit_id: food_potato.id)
-      create(:vote, exhibit_id: food_potato.id)
-
-      create(:vote, exhibit_id: food_fish.id)
-      create(:vote, exhibit_id: food_fish.id)
-      create(:vote, exhibit_id: food_fish.id)
+      2.times { create(:vote, exhibit_id: food_meat.id) }
+      3.times { create(:vote, exhibit_id: food_potato.id) }
+      4.times { create(:vote, exhibit_id: food_fish.id) }
+      5.times { create(:vote, exhibit_id: food_sake.id) }
     end
 
     it '投票数の多い順に並ぶ' do
-      expect(subject[0].id).to eq(food_fish.id)
-      expect(subject[1].id).to eq(food_potato.id)
-      expect(subject[2].id).to eq(food_meat.id)
+      expect(subject[0].id).to eq(food_sake.id)
+      expect(subject[1].id).to eq(food_fish.id)
+      expect(subject[2].id).to eq(food_potato.id)
+    end
+
+    it '2票以下はランキングに含まれない' do
+      expect(subject).not_to be_include(food_meat)
     end
   end
 end
