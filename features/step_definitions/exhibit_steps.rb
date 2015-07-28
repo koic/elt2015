8ba062fa0767764e38d472fb82e868f2f414b828
Yuka Kato <y-kato@esm.co.jp>
2015-07-28 23:32:02 +0900

    ランキングからコメント一覧へ遷移できることのテストを書いた

--- features/step_definitions/exhibit_steps.rb
+++ features/step_definitions/exhibit_steps.rb
@@ -3,6 +3,10 @@
   create(:food, title: food, user: user)
 end
 
+前提(/^料理 "(.*?)" がある$/) do |food|
+  create(:food, title: food, user: create(:user, nickname: 'alice'))
+end
+
 ならば(/^食べ物タブに "(.*?)" が表示されていること$/) do |food|
   within '.foods__table' do
     expect(page).to have_content food
