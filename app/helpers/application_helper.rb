2074f54abf4c1732a1cf5b5e22c2a22a7b678fa6
tatsuo.sakurai <tatsuo.sakurai@gmail.com>
2015-08-05 20:57:38 +0900

    Add exibit_image_tag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  IMAGE_SIZE = 64.freeze # image_url の表示サイズ
+
+  def exhibit_image_tag(image_url)
+    image_tag(image_url, hight: IMAGE_SIZE, width: IMAGE_SIZE) if image_url
+  end
 end
