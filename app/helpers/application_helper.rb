4dcfebb76469395d1f92120ff14e08d9c235103d
tatsuo.sakurai <tatsuo.sakurai@gmail.com>
2015-08-05 21:50:40 +0900

    Add link to image_url

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,7 @@ module ApplicationHelper
   IMAGE_SIZE = 64.freeze # image_url の表示サイズ
 
   def exhibit_image_tag(image_url)
-    image_tag(image_url, hight: IMAGE_SIZE, width: IMAGE_SIZE) if image_url
+    return unless image_url
+    link_to image_tag(image_url, hight: IMAGE_SIZE, width: IMAGE_SIZE), image_url
   end
 end
