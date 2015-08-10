ed694d60a7e20753c63dc937cb2eb59daa04d624
Yuka Kato <y-kato@esm.co.jp>
2015-08-10 14:58:57 +0900

    table を使ってステップを書くようにした

--- features/step_definitions/exhibit_steps.rb
+++ features/step_definitions/exhibit_steps.rb
@@ -20,6 +20,25 @@ end
   end
 end
 
+もし(/^以下の内容で出品物を登録する:$/) do |table|
+  # NOTE rows_hash は、テーブルヘッダが左端にくるタイプのテーブルで使う。
+  #      そうでないとき（ヘッダが上にくる）は、hashes を使う。
+  table.hashes.each do |row|
+    title = row.fetch('タイトル')
+    type = row.fetch('種別')
+    description = row.fetch('説明文', 'これは説明文です')
+
+    steps <<-EOS
+      もし 登録ページを表示する
+      かつ タイトルに "#{title}" と入力する
+      かつ 説明文に "#{description}" と入力する
+      かつ 種類として "#{type}" を選択する
+      かつ 登録ボタンを押す
+    EOS
+  end
+
+end
+
 もし(/^デバッグ$/) do
   binding.pry
 end
