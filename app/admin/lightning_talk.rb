f67a1ddc444d8276d1f95eaa7f88a71f3edee1e8
tatsuo.sakurai <tatsuo.sakurai@gmail.com>
2015-08-17 08:38:39 +0900

    ユーザとかタイプとかを選択できるようにした

--- app/admin/lightning_talk.rb
+++ app/admin/lightning_talk.rb
@@ -1,3 +1,15 @@
 ActiveAdmin.register LightningTalk do
-  permit_params LightningTalk.attribute_names - %w[id created_at updated_at]
+  attribute_names = LightningTalk.attribute_names - %w[id created_at updated_at]
+  permit_params attribute_names
+
+  form do |f|
+    f.inputs do
+      f.input :user, label: 'ユーザ', as: :select, collection: User.all.map {|u| ["#{u.nickname}", u.id]}
+      (attribute_names - %w[user_id type votes_count]).each do |attribute_name|
+        f.input attribute_name
+      end
+    end
+
+    f.actions
+  end
 end
--- app/admin/vote.rb
+++ app/admin/vote.rb
@@ -1,3 +1,16 @@
 ActiveAdmin.register Vote do
-  permit_params Vote.attribute_names - %w[id created_at updated_at]
+  attribute_names = Vote.attribute_names - %w[id created_at updated_at]
+  permit_params attribute_names
+
+  form do |f|
+    f.inputs do
+      f.input :user, label: 'ユーザ', as: :select, collection: User.all.map {|u| ["#{u.nickname}", u.id]}
+      f.input :exhibit, label: '出品物', as: :select, collection: Exhibit.all.map {|e| ["#{e.title}", e.id]}
+      (attribute_names - %w[user_id exhibit_id]).each do |attribute_name|
+        f.input attribute_name
+      end
+    end
+
+    f.actions
+  end
 end
