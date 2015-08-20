9da93c1263cc2e9920417a71ba6d68181483a556
Yuka Kato <y-kato@esm.co.jp>
2015-08-21 01:39:53 +0900

    ユーザが自分の投票を削除できるようにした

--- app/controllers/my/votes_controller.rb
+++ app/controllers/my/votes_controller.rb
@@ -2,4 +2,11 @@ class My::VotesController < ApplicationController
   def index
     @votes = current_user.votes.preload(exhibit: :user)
   end
+
+  def destroy(id)
+    vote = current_user.votes.find(id)
+    vote.destroy
+
+    redirect_to my_votes_path, notice: '削除しました。'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
   namespace :my do
     resources :exhibits, only: %i(index edit update destroy)
-    resources :votes,    only: %i(index)
+    resources :votes,    only: %i(index destroy)
   end
 
   scope module: :exhibit do
