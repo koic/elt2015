a59fe98b5df9a44c48acfd23caa9e8713c340adf
Yuka Kato <y-kato@esm.co.jp>
2015-08-21 02:31:01 +0900

    ユーザが自分の投票のコメント内容を編集・更新できるようにした

--- app/controllers/my/votes_controller.rb
+++ app/controllers/my/votes_controller.rb
@@ -1,6 +1,22 @@
 class My::VotesController < ApplicationController
+  permits :comment
+
   def index
     @votes = current_user.votes.preload(exhibit: :user)
+  end
+
+  def edit(id)
+    @vote = current_user.votes.find(id)
+  end
+
+  def update(id, vote)
+    @vote = current_user.votes.find(id)
+
+    if @vote.update(vote)
+      redirect_to my_votes_path, notice: '更新しました。'
+    else
+      render :edit
+    end
   end
 
   def destroy(id)
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
   namespace :my do
     resources :exhibits, only: %i(index edit update destroy)
-    resources :votes,    only: %i(index destroy)
+    resources :votes,    only: %i(index edit update destroy)
   end
 
   scope module: :exhibit do
