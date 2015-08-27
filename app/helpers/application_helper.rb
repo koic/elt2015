cf0b7371c8284773973c933967d3f928d8d5b081
tatsuo.sakurai <tatsuo.sakurai@gmail.com>
2015-08-27 10:03:29 +0900

    LTはスライド、Foodは画像を表示するようにした（要横展開）

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,20 @@
 module ApplicationHelper
   IMAGE_SIZE = 64.freeze # image_url の表示サイズ
 
+  def exhibit_image_or_slide_url_tag(exhibit)
+    exhibit_image_tag(exhibit.image_url) if exhibit.image_url
+    exhibit_slide_tag(exhibit.slide_url) if exhibit.slide_url
+  end
+
   def exhibit_image_tag(image_url)
     return unless image_url
     link_to image_tag(image_url, hight: IMAGE_SIZE, width: IMAGE_SIZE), image_url
+  end
+
+  def exhibit_slide_tag(slide_url)
+    return unless slide_url
+    link_to slide_url, target: :blank do
+      '<i class="fa fa-youtube-play"></i>'.html_safe
+    end
   end
 end
